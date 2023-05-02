c5079772df66912aa1d39933392e34419459c422
SGM2099 <sebasguti1511@comunidad.unam.mx>
2023-05-01 23:15:17 -0600

    Pantalla de creditos sin botones

---
+++ app/src/main/java/mx/unam/fciencias/moviles/proyectoar/Creditos.java
@@ -0,0 +1,15 @@
+package mx.unam.fciencias.moviles.proyectoar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Creditos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creditos);
+    }
+
+}
