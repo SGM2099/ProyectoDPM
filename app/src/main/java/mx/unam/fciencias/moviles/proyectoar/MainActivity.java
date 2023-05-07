5263f72e3cf9107d36291ee71a2e687bd27cd133
SlemimanPzz <emilianoa@ciencias.unam.mx>
2023-05-06 19:10:47 -0600

    DeepAr loads

--- app/src/main/java/mx/unam/fciencias/moviles/proyectoar/MainActivity.java
+++ app/src/main/java/mx/unam/fciencias/moviles/proyectoar/MainActivity.java
@@ -1,14 +1,119 @@
 package mx.unam.fciencias.moviles.proyectoar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
 
-public class MainActivity extends AppCompatActivity {
+import ai.deepar.ar.ARErrorType;
+import ai.deepar.ar.AREventListener;
+import ai.deepar.ar.DeepAR;
+
+
+public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback, AREventListener {
+
+    private DeepAR deepAR;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initializeDeepAR();
         setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    protected void onStart() {
+
+        super.onStart();
+    }
+
+
+
+    private void initializeDeepAR() {
+        deepAR = new DeepAR(this);
+        deepAR.setLicenseKey(BuildConfig.deepar_api_key);
+        deepAR.initialize(this, this);
+        Log.i("DeepAR Initializer", "Deep AR initialize");
+    }
+
+
+    @Override
+    public void screenshotTaken(Bitmap bitmap) {
+
+    }
+
+    @Override
+    public void videoRecordingStarted() {
+
+    }
+
+    @Override
+    public void videoRecordingFinished() {
+
+    }
+
+    @Override
+    public void videoRecordingFailed() {
+
+    }
+
+    @Override
+    public void videoRecordingPrepared() {
+
+    }
+
+    @Override
+    public void shutdownFinished() {
+
+    }
+
+    @Override
+    public void initialized() {
+
+    }
+
+    @Override
+    public void faceVisibilityChanged(boolean b) {
+
+    }
+
+    @Override
+    public void imageVisibilityChanged(String s, boolean b) {
+
+    }
+
+    @Override
+    public void frameAvailable(Image image) {
+
+    }
+
+    @Override
+    public void error(ARErrorType arErrorType, String s) {
+
+    }
+
+    @Override
+    public void effectSwitched(String s) {
+
+    }
+
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
+
     }
 }
