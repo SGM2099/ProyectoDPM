17f8a7d263cc5b749efd4355edfcb82d65d69f85
SGM2099 <sebasguti1511@comunidad.unam.mx>
2023-05-22 01:08:16 -0600

    Layout basico de ventana Informacion

---
+++ app/src/main/java/mx/unam/fciencias/moviles/proyectoar/Informacion.java
@@ -0,0 +1,14 @@
+package mx.unam.fciencias.moviles.proyectoar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Informacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_informacion);
+    }
+}
